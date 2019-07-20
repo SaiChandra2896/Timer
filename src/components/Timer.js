a429cb2183126ef5adc19395f4b9c5bc1cc7fa1b
SaiChandra2896 <saichandra2896@gmail.com>
2019-07-20 17:45:08 +0530

    add onclick event to start timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -12,9 +12,20 @@ class Timer extends Component {
       isOn: false
     }
   }
+
+  startTimer = (e) => {
+    this.setState({
+      time: this.state.time,
+      start: Date.now() - this.state.time,
+      isOn: true
+    });
+    this.timer = setInterval(() => {
+      this.setState({ time: Date.now() - this.state.start })
+    }, 1000);
+  }
   render() {
     let start = (this.state.time === 0) ?
-      <Button className='btn btn-large btn-success' onClick={this.onClick}>Start</Button> : null;
+      <Button className='btn btn-large btn-success' onClick={this.startTimer}>Start</Button> : null;
 
     let stop = (this.state.isOn === true) ?
       <Button className='btn btn-large btn-danger' onClick={this.stopTimer}>Stop</Button> : null;
@@ -25,8 +36,9 @@ class Timer extends Component {
     let resume = (this.state.time !== 0 && this.state.isOn === false) ?
       <Button className='btn btn-large btn-warning' onClick={this.resumeTimer}>Resume</Button> : '';
     return (
-      <div className='timer mt-5'>
+      <div className='timer mt-5 rounded-0'>
         <h1>
+          <h3>Timer: {this.state.time}</h3>
           {start}
           {stop}
           {reset}
